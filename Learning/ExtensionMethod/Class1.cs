6f3e12aabfcc8dd830cb4135c2c7521ff71cc148
bunny <banhibikashsaha@gmail.com>
2026-01-18 01:03:01 +0530

    added a extension program along with use of Action delegate

---
+++ Learning/ExtensionMethod/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    static class ExtensionCLass
+    {
+        public static void mul(this Original x,int a, int b)
+        {
+            Console.WriteLine("\nThe product of {0} and {1} is {2}.",a,b,(a*b));
+            x.say();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning/ExtensionMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    class Original
+    {
+        protected void add(int a,int b)
+        {
+            Console.WriteLine("\nThe sum is " + (a + b));
+            Console.ReadLine();
+        }
+
+        public void say()
+        {
+            Console.WriteLine("\nWe are in Original class.");
+        }
+    }
+    internal class Program:Original
+    {
+        static void Main(string[] args)
+        {
+            Program o = new Program();
+            Action<int, int> obj = o.add;
+            obj += o.mul;
+            obj(3, 5);
+        }
+    }
+}
