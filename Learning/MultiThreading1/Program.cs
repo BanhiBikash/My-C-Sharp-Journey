912682b5611803b624625d698b3d5a3f44757590
bunny <banhibikashsaha@gmail.com>
2026-01-18 18:13:52 +0530

    created a simple multi-threading program

---
+++ Learning/MultiThreading1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreading1
+{
+    internal class Program
+    {
+        public void Count1()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\nThread 1: "+i);
+            }
+        }
+        public void Count2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\nThread 2: " + i);
+
+                if (i == 3)
+                {
+                    Console.WriteLine("\nThread2 going to sleep.");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("\nThread2 is awakening.");
+                }
+            }
+        }
+        public void Count3()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\nThread 3: " + i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+
+            Thread t1 = new Thread(p.Count1);
+            Thread t2 =new Thread(p.Count2);
+            Thread t3=  new Thread(p.Count3);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
---
+++ Learning/MultiThreading1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MultiThreading1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultiThreading1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d65414fb-03e5-4110-9b55-0ad0fc64ebc3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
