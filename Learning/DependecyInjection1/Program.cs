d2ed0df787314e7a6a5e9d4e1e0ea5e2a4ba11ec
bunny <banhibikashsaha@gmail.com>
2026-01-19 09:12:39 +0530

    Created a builder dependency injection program

---
+++ Learning/DependecyInjection1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependecyInjection1
+{
+    internal class Hammer
+    {
+        public void use()
+        {
+            Console.WriteLine("\nHammering Nails! Bang.....");
+        }
+    }
+
+    internal class Saw
+    {
+        public void use()
+        {
+            Console.WriteLine("\nSawing wood! Gr.......");
+        }
+    }
+
+    internal class BuildHouse
+    {
+        public BuildHouse(Hammer h, Saw s)
+        {
+            try
+            {
+                h.use();
+                s.use();
+                Console.WriteLine("\n Yay! Our new home is ready!");
+            }
+            catch (Exception e) {
+                Console.WriteLine("\n House isn't built! :(");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }     
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hammer h =new Hammer();
+            Saw s  = new Saw();
+            BuildHouse build = new BuildHouse(h, s);
+        }
+    }
+}
---
+++ Learning/MultiThreadingDelegates/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadingDelegates
+{
+    internal class Program
+    {
+        //non-parametrized
+        public void test1()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("\nTest 1: "+i);
+            }
+            Console.WriteLine("\nEnd of test1.");
+        }
+
+        //parametrized
+        public void test2(object max)
+        {
+            for(int i=0;i<= Convert.ToInt32(max); i++)
+            {
+                Console.WriteLine("\nTest 2:" + i);
+                if (Convert.ToInt32(max) == 2)
+                {
+                    Console.WriteLine("\nThread2 going to sleep.");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("\nThread2 coming out of sleep.");
+                }
+            }
+            Console.WriteLine("\nEnd of test2.");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            ThreadStart obj = p.test1;
+            Thread t1 = new Thread(obj);
+
+            ParameterizedThreadStart obj2 = p.test2;
+            Thread t2 = new Thread(obj2);
+
+            //starting both threads
+            t1.Start();
+            t2.Start(12);
+            Console.WriteLine("\nEnd of main.");
+        }
+    }
+}
